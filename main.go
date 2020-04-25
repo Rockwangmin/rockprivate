2fe6092711231887e7bc86e4cf762a5a20b7a8a2
Rockwangmin <1047063106@qq.com>
2020-04-25 14:43:11 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2020 Min Wang <min.m.wang@ericsson.com>
+This file is part of {{ .appName }}.
+*/
+package main
+
+import "ericsson.com/iot/iot-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
